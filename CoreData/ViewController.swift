64ffe63dcce9e529b9dbe0cc7b6bd0d8391c730a
Anirudha <anirudhamusic.am@gmail.com>
2017-11-02 11:02:24 +0530

    Initial commit

---
+++ CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Anirudha on 02/11/17.
+//  Copyright © 2017 Anirudha Mahale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
