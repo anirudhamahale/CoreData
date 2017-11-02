9dd35a192b02a845758b4635b27760cf4a8ff301
Anirudha <anirudhamusic.am@gmail.com>
2017-11-02 11:11:00 +0530

    did small change.

---
+++ CoreData/PersonTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonTableViewCell.swift
+//  CoreData
+//
+//  Created by Anirudha on 02/11/17.
+//  Copyright © 2017 Anirudha Mahale. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    
+}
